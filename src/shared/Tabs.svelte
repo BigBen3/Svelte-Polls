bec96ae67c317e6bb87a7665d37507c843f1c97a
BigBen3 <benjamin.s.sandler@gmail.com>
2023-03-10 11:00:51 -0800

    not complete

---
+++ src/shared/Tabs.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let items;
+    export let activeItem;
+</script>
+
+<div class="tabs"></div>
+    <ul>
+        {#each items as item}
+        <li>
+            
+        </li>
+    </ul>
+
+<style>
+
+</style>
