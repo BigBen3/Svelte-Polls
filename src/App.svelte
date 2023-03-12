2a7973ab38a11ff939671f307a52e4bf25b778d4
BigBen3 <benjamin.s.sandler@gmail.com>
2023-03-12 13:54:47 -0700

    video 20 code adding reusable components

--- src/App.svelte
+++ src/App.svelte
@@ -6,10 +6,18 @@
 	let items = ['Current Polls', 'Add New Poll'];
 	let activeItem = 'Current Polls';
 
+	const tabChange = (e) => {
+		activeItem = e.detail;
+	}
 </script>
 <Header />
 <main>
-<Tabs {activeItem} {items}/>
+	<Tabs {activeItem} {items} on:tabChange={tabChange}/>
+	{#if activeItem === 'Current Polls'}
+		<p>Poll list componenet goes here</p>
+	{:else if activeItem === 'Add New Poll'}
+		<p>New poll form componenet</p>
+	{/if}
 </main>
 <Footer />
 <style>
