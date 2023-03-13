41b39869fc150fa45be7bb02ad558f4d8c86f472
BigBen3 <benjamin.s.sandler@gmail.com>
2023-03-13 09:53:28 -0700

    video 21

--- src/components/CreatePollForm.svelte
+++ src/components/CreatePollForm.svelte
@@ -1,24 +1,43 @@
-<sript>
+<script>
+    let fields = {question: '', answerA: '', answerB: ''};
 
+    const submitHandler = () => {
+        console.log(fields);
+    }
+</script>
 
-</sript>
-
-<form>
+<form on:submit|preventDefault={submitHandler}>
     <div class="form-field">
         <label for="question">Poll Question:</label>
-        <input type="text" id="question">
+        <input type="text" id="question" bind:value={fields.question}>
     </div>
     <div class="form-field">
         <label for="answer-a">Answer A:</label>
-        <input type="text" id="answer-a">
+        <input type="text" id="answer-a"bind:value={fields.answerA}>
     </div>
     <div class="form-field">
         <label for="answer-b">Answer B:</label>
-        <input type="text" id="answer-b">
+        <input type="text" id="answer-b" bind:value={fields.answerB}>
     </div>
+    <button>Add Poll</button>
 </form>
 
 
 <style>
-
+    form{
+        width: 400px;
+        margin: 0 auto;
+        text-align: center;
+    }
+    .form-field{
+        margin: 18px auto;
+    }
+    input{
+        width: 100%;
+        border-radius: 6px;
+    }
+    label{
+        margin: 10x auto;
+        text-align: left;
+    }
 </style>
