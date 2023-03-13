b97c0f85e346eda2bd554d942a8dbd43bc0033be
BigBen3 <benjamin.s.sandler@gmail.com>
2023-03-13 12:50:30 -0700

    video 22 completed

--- src/components/CreatePollForm.svelte
+++ src/components/CreatePollForm.svelte
@@ -1,43 +1,44 @@
 <script>
-    let fields = {question: '', answerA: '', answerB: ''};
-
-    const submitHandler = () => {
-        console.log(fields);
+    import Button from '../shared/Button.svelte';
+  
+    let fields = { question: '', answerA: '', answerB: '' };
+  
+    let submitHandler = () => {
+      console.log(fields);
     }
-</script>
-
-<form on:submit|preventDefault={submitHandler}>
+  </script>
+  
+  <form on:submit|preventDefault={submitHandler}>
     <div class="form-field">
-        <label for="question">Poll Question:</label>
-        <input type="text" id="question" bind:value={fields.question}>
+      <label for="question">Poll Question:</label>
+      <input type="text" id="question" bind:value={fields.question}>
     </div>
     <div class="form-field">
-        <label for="answer-a">Answer A:</label>
-        <input type="text" id="answer-a"bind:value={fields.answerA}>
+      <label for="answer-a">Answer A value:</label>
+      <input type="text" id="answer-a" bind:value={fields.answerA}>
     </div>
     <div class="form-field">
-        <label for="answer-b">Answer B:</label>
-        <input type="text" id="answer-b" bind:value={fields.answerB}>
+      <label for="answer-b">Answer B value:</label>
+      <input type="text" id="answer-b" bind:value={fields.answerB}>
     </div>
-    <button>Add Poll</button>
-</form>
-
-
-<style>
+    <Button>Add Poll</Button>
+  </form>
+  
+  <style>
     form{
-        width: 400px;
-        margin: 0 auto;
-        text-align: center;
+      width: 400px;
+      margin: 0 auto;
+      text-align: center;
     }
     .form-field{
-        margin: 18px auto;
+      margin: 18px auto;
     }
     input{
-        width: 100%;
-        border-radius: 6px;
+      width: 100%;
+      border-radius: 6px;
     }
     label{
-        margin: 10x auto;
-        text-align: left;
+      margin: 10px auto;
+      text-align: left;
     }
-</style>
+  </style>
