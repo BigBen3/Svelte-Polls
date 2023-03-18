0d11e0badb8ddd85398d6cfb5a6999a476c8be96
BigBen3 <benjamin.s.sandler@gmail.com>
2023-03-17 19:34:06 -0700

    fixed some random error nothign was loading on the page

--- src/components/PollList.svelte
+++ src/components/PollList.svelte
@@ -1,28 +1,27 @@
+
 <script>
-    import PollStore from '../stores/PollStore';
+    import PollStore from '../stores/pollStore.js';
     import PollDetails from './PollDetails.svelte';
-    export let polls = [];
-
-    PollStore.suscribe(data => {
-        polls = data;
-    })
-</script>
-
-<div class="poll-list">
+    let polls = [];
+    PollStore.subscribe(data => {
+      console.log(data);
+      polls = data;
+    });
+  </script>
+  
+  <div class="poll-list">
     {#each polls as poll (poll.id)}
-        <div>
-            <!--so from here we foward the click event we sent from the poll list to the app .svelte-->
-              <PollDetails {poll} on:vote/>
-        
-        </div>
+      <div>
+          <!--so from here we foward the click event we sent from the poll list to the app .svelte-->
+        <PollDetails {poll} on:vote />
+      </div>
     {/each}
-</div>
-
-<!--1fr makes each grid item take half the space of the div-->
-<style>
+  </div>
+  <!--1fr makes each grid item take half the space of the div-->
+  <style>
     .poll-list{
-        display:grid;
-        grid-template-columns: 1fr 1fr;
-        grid-gap: 20px;
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      grid-gap: 20px;
     }
-</style>
+  </style>
